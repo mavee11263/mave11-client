3b3aed4148bc2cc8f6d8a8fc560ecbecb3002995
tatendabakozw <tatendabakozw@gmail.com>
2022-07-29 12:07:35 +0200

    feat: upload picture to firebase

--- components/SingleVideo/SingleVideo.tsx
+++ components/SingleVideo/SingleVideo.tsx
@@ -1,22 +1,33 @@
 import { Text } from "@chakra-ui/react";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import React from "react";
 
-interface Props{
-  _id:string,
-  title: string,
-  likes: number,
-  numberOfViews: number,
-  thumbnail:string
+interface Props {
+  _id: string;
+  title: string;
+  likes: number;
+  numberOfViews: number;
+  thumbnail: string;
 }
 
-function SingleVideo({_id, title, likes, numberOfViews, thumbnail}:Props) {
-  const router = useRouter()
+function SingleVideo({ _id, title, likes, numberOfViews, thumbnail }: Props) {
+  const router = useRouter();
   return (
-    <div onClick={()=> router.push(`/video/${_id}`)} className="col-span-1 cursor-pointer flex flex-col">
-      <div className="relative">
-        {/* <Image src={image_video} layout="responsive" objectFit="cover" /> */}
-        <img src={thumbnail} alt="video thumbnail" className=" object-cover" />
+    <div
+      onClick={() => router.push(`/video/${_id}`)}
+      className="col-span-1 cursor-pointer flex flex-col"
+    >
+      <div className="relative md:h-44 h-56 overflow-hidden">
+        <Image
+          quality={50}
+          placeholder="blur"
+          blurDataURL={thumbnail}
+          src={thumbnail}
+          layout="fill"
+          objectFit="cover"
+        />
+        {/* <img src={thumbnail} alt="video thumbnail" className=" object-cover" /> */}
       </div>
       <div className="flex flex-col md:px-0 px-2">
         <Text
@@ -28,15 +39,17 @@ function SingleVideo({_id, title, likes, numberOfViews, thumbnail}:Props) {
         </Text>
       </div>
       <div className="flex flex-row items-center justify-left space-x-2 dark:text-gray-500 text-gray-500 text-sm py-2 md:px-0 px-2">
-        <Text>{numberOfViews} {numberOfViews < 1 ? "View" : "Views"}</Text>
+        <Text>
+          {numberOfViews} {numberOfViews < 1 ? "View" : "Views"}
+        </Text>
         <span className="text-xl">&#183;</span>
-        {
-          likes < 1 ?(
-            <Text></Text>
-          ):(
-            <Text>{likes} {likes > 1 ? "likes" : "like"}</Text>
-          )
-        }
+        {likes < 1 ? (
+          <Text></Text>
+        ) : (
+          <Text>
+            {likes} {likes > 1 ? "likes" : "like"}
+          </Text>
+        )}
       </div>
     </div>
   );
