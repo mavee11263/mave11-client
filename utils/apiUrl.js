555a03f983202a9ead6d896b6c44b4a19c45d6ae
tatendabakozw <tatendabakozw@gmail.com>
2022-07-29 10:20:44 +0200

    fix: chaged api url

--- utils/apiUrl.js
+++ utils/apiUrl.js
@@ -1,2 +1,5 @@
-export const apiUrl = `http://localhost:5500`
-export const socketUrl =`http://localhost:5500`
+// export const apiUrl = `http://localhost:5500`
+// export const socketUrl =`http://localhost:5500`
+
+export const apiUrl = `https://mavee11.herokuapp.com`
+export const socketUrl = `https://mavee11.herokuapp.com`
