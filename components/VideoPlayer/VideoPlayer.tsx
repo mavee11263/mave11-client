bb18fd90b4bb5eda18c6c8aa74081c77f1a448cd
tatendabakozw <tatendabakozw@gmail.com>
2022-08-30 10:04:30 +0200

    fix: download on login

--- components/VideoPlayer/VideoPlayer.tsx
+++ components/VideoPlayer/VideoPlayer.tsx
@@ -1,4 +1,6 @@
+import { useContext } from "react";
 import ReactPlayer from "react-player/lazy";
+import { Store } from "../../Context/Store";
 
 interface Props {
   video_url?: string;
@@ -6,22 +8,39 @@ interface Props {
 }
 
 const VideoPlayer = ({ video_url, thumbnail }: Props) => {
+  const { state } = useContext(Store);
+  const { mavee_11_user } = state;
   return (
     <div className="flex lg:h-[550px] md:h-[350px] h-[200px] w-full flex-col items-center bg-black">
-      <ReactPlayer
-        // Disable download button
-        config={{ file: { attributes: { controlsList: "nodownload" } } }}
-        // Disable right click
-        onContextMenu={(e: any) => e.preventDefault()}
-        // Your props
-        url={video_url}
-        playing={true}
-        className="react-player"
-        light={thumbnail}
-        controls
-        width="100%"
-        height="100%"
-      />
+      {mavee_11_user ? (
+        <ReactPlayer
+          // Disable right click
+          onContextMenu={(e: any) => e.preventDefault()}
+          // Your props
+          url={video_url}
+          playing={true}
+          className="react-player"
+          light={thumbnail}
+          controls
+          width="100%"
+          height="100%"
+        />
+      ) : (
+        <ReactPlayer
+          // Disable download button
+          config={{ file: { attributes: { controlsList: "nodownload" } } }}
+          // Disable right click
+          onContextMenu={(e: any) => e.preventDefault()}
+          // Your props
+          url={video_url}
+          playing={true}
+          className="react-player"
+          light={thumbnail}
+          controls
+          width="100%"
+          height="100%"
+        />
+      )}
     </div>
   );
 };
